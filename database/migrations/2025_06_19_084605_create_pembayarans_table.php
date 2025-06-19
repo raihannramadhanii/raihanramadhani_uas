bb51f4198f2218ed80048beb58edd46db7f5ba23
raihannramadhanii <raihannramadhanii@gmail.com>
2025-06-19 16:47:25 +0700

    menambahkan table pembayarans

---
+++ database/migrations/2025_06_19_084605_create_pembayarans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembayarans', function (Blueprint $table) {
+            $table->id();
+            $table->string('kd_pembayaran');
+            $table->bigInteger('penjualans_id');
+            $table->string('total_pembayaran');
+            $table->string('jns_pembayaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayarans');
+    }
+};
