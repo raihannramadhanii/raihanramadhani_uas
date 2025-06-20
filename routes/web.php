0dcc732d4657a3903b9994d9dadf818e1e546ffe
raihannramadhanii <raihannramadhanii@gmail.com>
2025-06-20 11:23:29 +0700

    masukin template

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/cektemplate', function () {
+    return view('layouts.template');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
